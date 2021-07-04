9b93eeafeee92afd8f953717a81adfe4e73b9ba9
Maxim Musayev <shamanu4@gmail.com>
2021-07-04 13:43:46 +0300

    Update demo project

--- demo_project/notes/models.py
+++ demo_project/notes/models.py
@@ -1,10 +1,7 @@
 # -*- encoding: utf-8 -*-
-from __future__ import unicode_literals
-from six import python_2_unicode_compatible
 from django.db import models
 
 
-@python_2_unicode_compatible
 class Country(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
@@ -12,7 +9,6 @@ class Country(models.Model):
         return self.name
 
 
-@python_2_unicode_compatible
 class Person(models.Model):
     name = models.CharField(max_length=100, unique=True)
     from_country = models.ForeignKey(Country, on_delete=models.CASCADE)
@@ -21,7 +17,6 @@ class Person(models.Model):
         return self.name
 
 
-@python_2_unicode_compatible
 class Asset(models.Model):
     name = models.CharField(max_length=100, unique=True)
     from_country = models.ForeignKey(Country, on_delete=models.CASCADE)
